0f3406b27f2fb42078184601005a8ec46dc4dd74
Angeline <ammeitzler@gmail.com>
2019-07-22 06:10:20 -0500

    added phonenumbers library

--- sort.py
+++ sort.py
@@ -2,16 +2,21 @@ import re
 import nltk
 import sys
 from nltk.corpus import stopwords
+import phonenumbers
 
 matches = ""
-# text = "this is a tes (248)698-0988 so is this 1233211234 and this 123-123-2222"
+# text = "this is a tes (248)698-0988 so is this 510-748-8230 if it's before 9:30, or on 703-4800500 "
 # print(sys.argv[1])
 text = sys.argv[1]
 
 def extract_phone_numbers(string):
-    r = re.compile(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})')
-    phone_numbers = r.findall(string)
-    return [re.sub(r'\D', '', number) for number in phone_numbers]
+	match_array = []
+	for match in phonenumbers.PhoneNumberMatcher(string, "US"):
+		match_array.append(match)
+	return match_array
+    # r = re.compile(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})')
+    # phone_numbers = r.findall(string)
+    # return [re.sub(r'\D', '', number) for number in phone_numbers]
 
 def main(text):
 	global matches
